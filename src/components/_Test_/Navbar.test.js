4a9e5072070befebc098943e71ee4b293b0d11d3
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-08-17 16:09:44 +0200

    added failing test

--- src/components/_Test_/Navbar.test.js
+++ src/components/_Test_/Navbar.test.js
@@ -1,7 +1,14 @@
-import renderer from 'react-test-renderer';
-import Navbar from '../Navbar'
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../Navbar';
 
-it('Navbar render correctly', ()=>{
- const tree = renderer.create(<Navbar />).toJSON()
- expect(tree).toMatchSnapshot();
-})
+it('Navbar render correctly', () => {
+  render(
+    <Router>
+      const tree = renderer.create(
+      <Navbar />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    </Router>,
+  );
+});
--- src/components/_Test_/calculate.test.js
+++ src/components/_Test_/calculate.test.js
@@ -1,45 +1,45 @@
 import calculate from '../../logic/Calculate';
 
-describe('Tests on calculator.js calculate function', ()=>{
-    test('Test AC button on calculator', ()=>{
-        const result = calculate({ total: '777', next: '75', operation: '' }, 'AC');
-        expect(result).toStrictEqual({ total: null, next: null, operation: null });
-    })
+describe('Tests on calculator.js calculate function', () => {
+  test('Test AC button on calculator', () => {
+    const result = calculate({ total: '777', next: '75', operation: '' }, 'AC');
+    expect(result).toStrictEqual({ total: null, next: null, operation: null });
+  });
 
-    test('Test = button on calculator', ()=>{
-        const result = calculate({ total: '70', next: '75', operation: 'x'  }, '=');
-        expect(result).toStrictEqual({ total: '5250', next: null, operation: null });
-    })
+  test('Test = button on calculator', () => {
+    const result = calculate({ total: '70', next: '75', operation: 'x' }, '=');
+    expect(result).toStrictEqual({ total: '5250', next: null, operation: null });
+  });
 
-    test('Test +/- button on calculator', ()=>{
-        const plusMinus = calculate({ total: '70', next: null, operation: null  }, '+/-');
-        expect(plusMinus).toStrictEqual({ total: '-70', next: null, operation: null });
-    })
+  test('Test +/- button on calculator', () => {
+    const plusMinus = calculate({ total: '70', next: null, operation: null }, '+/-');
+    expect(plusMinus).toStrictEqual({ total: '-70', next: null, operation: null });
+  });
 
-    test('Test dot button on calculator', ()=>{
-        let dotSsign = calculate({ total: null, next: null, operation: null  }, '5');
-        dotSsign = calculate(dotSsign, '.')
-        dotSsign = calculate(dotSsign, '70')
-        expect(dotSsign).toStrictEqual({ total: null, next: '5.70', });
-    })
+  test('Test dot button on calculator', () => {
+    let dotSsign = calculate({ total: null, next: null, operation: null }, '5');
+    dotSsign = calculate(dotSsign, '.');
+    dotSsign = calculate(dotSsign, '70');
+    expect(dotSsign).toStrictEqual({ total: null, next: '5.70' });
+  });
 
-    test('Test - button on calculator', ()=>{
-        const Minus = calculate({ total: '70', next: '90', operation: '-'  },  'x');
-        expect(Minus).toStrictEqual({ total: '-20', next: null, operation: 'x' });
-    })
+  test('Test - button on calculator', () => {
+    const Minus = calculate({ total: '70', next: '90', operation: '-' }, 'x');
+    expect(Minus).toStrictEqual({ total: '-20', next: null, operation: 'x' });
+  });
 
-    test('Test + button on calculator', ()=>{
-        const plus = calculate({ total: '70', next: '90', operation: '+'  },  '+');
-        expect(plus).toStrictEqual({ total: '160', next: null, operation: '+' });
-    })
+  test('Test + button on calculator', () => {
+    const plus = calculate({ total: '70', next: '90', operation: '+' }, '+');
+    expect(plus).toStrictEqual({ total: '160', next: null, operation: '+' });
+  });
 
-    test('Test ÷ button on calculator', ()=>{
-        const plus = calculate({ total: '100', next: '50', operation: '÷'  },  '=');
-        expect(plus).toStrictEqual({ total: '2', next: null, operation: null });
-    })
+  test('Test ÷ button on calculator', () => {
+    const plus = calculate({ total: '100', next: '50', operation: '÷' }, '=');
+    expect(plus).toStrictEqual({ total: '2', next: null, operation: null });
+  });
 
-    test('Test % button on calculator', ()=>{
-        const result = calculate({ total: null, next: '1200', operation: null }, '%');
+  test('Test % button on calculator', () => {
+    const result = calculate({ total: null, next: '1200', operation: null }, '%');
     expect(result).toStrictEqual({ total: '1200', next: null, operation: '%' });
-    })
-})
+  });
+});
