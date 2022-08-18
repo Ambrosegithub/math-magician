6f04cbdf4cfe29dc26e0eed683b9ba6ae3004553
Isigba Ambrose <ambrisexyz@gmail.com>
2022-08-18 14:24:46 +0200

    make changes in line with reviewer request

--- src/components/_Test_/calculator.test.js
+++ src/components/_Test_/calculator.test.js
@@ -1,7 +1,28 @@
-import renderer from 'react-test-renderer';
-import Calculator from '../calculator';
+import calculate from '../../logic/Calculate';
 
-it('calculator render correctly', () => {
-  const tree = renderer.create(<Calculator />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Renders Calculator component', () => {
+  test('test calculate objects and test contents', () => {
+    const obj1 = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    const obj2 = {
+      total: '0',
+      next: '0',
+      operation: '+',
+    };
+
+    const buttonName1 = 'AC';
+    const buttonName2 = '0';
+
+    expect(calculate(obj1, buttonName1)).toMatchObject({
+      total: null,
+      next: null,
+      operation: null,
+    });
+
+    expect(calculate(obj2, buttonName2)).toMatchObject({});
+  });
 });
